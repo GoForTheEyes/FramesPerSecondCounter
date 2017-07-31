46133326e81c28c9362dd272dfff98ebc2dfc658
GoForTheEyes <brabandere@gmail.com>
2017-07-30 23:41:49 -0400

    FPS counter updating per frame
    FPS counter appears on upper left panel and updates every frame.

---
+++ Assets/FPSCounter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour {
+
+    public int FPS { get; private set; }
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //we used unscaledDeltaTime instead of deltaTime in case the time scale of the unity editor is not 1s 
+        FPS = (int)(1f / Time.unscaledDeltaTime);
+	}
+}
